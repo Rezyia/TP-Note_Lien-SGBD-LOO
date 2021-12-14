87907aedce0c64d88b7e9a2644438cd065bc3049
theo-boi <boisseau.theo@gmail.com>
2021-12-14 11:30:09 +0100

    aa

--- src/app/DBConnection.java
+++ src/app/DBConnection.java
@@ -1,6 +1,5 @@
 package app;
 import java.sql.*;
-import java.time.LocalDate;
 import java.util.Properties;
 
 public class DBConnection {
@@ -18,7 +17,7 @@ public class DBConnection {
 		connected = connect();
 		if (connected) {
 			dbTableInit();
-			dbTuplesInit();	
+			//dbTuplesInit();	
 		}
 	}
 	
@@ -57,52 +56,13 @@ public class DBConnection {
 	 * @throws SQLException
 	 */
 	private void dbTableInit() {		
-		String ctLivre = "if not exists create table Livre("
-				+ "id int not null,"
-				+ "titre varchar(40),"
-				+ "auteur int,"
-				+ "dateParution Date,"
-				+ "primary key (id),"
-				+ "foreign key (id) references Auteur(id);"; 
-		
-		String ctExemplaire = "if not exists create table Exemplaire("
-				+ "id int not null,"
-				+ "livre int,"
-				+ "texte varchar(2048),"
-				+ "primary key (id)),"
-				+ "foreign key (livre) references Livre(id);";
-		
-		String ctAuteur = "if not exists create table Auteur("
-				+ "id int not null,"
-				+ "nom varchar(30),"
-				+ "prenom varchar(30),"
-				+ "primary key (id));";
-		
-		String ctAssocie = "if not exists create table Associe("
-				+ "id int not null,"
-				+ "nom varchar(30),"
-				+ "prenom varchar(30),"
-				+ "primary key (id));";
-		
-		String ctPret = "if not exists create table Pret("
-				+ "id int not null,"
-				+ "exemplaire int not null,"
-				+ "associe int not null,"
-				+ "dateEmprunt Date not null,"
-				+ "dateRestitution Date,"
-				+ "dureeMax int not null," // Durée max de l'emprunt en jours
-				+ "primary key (id),"
-				+ "foreign key (exemplaire) references Exemplaire(id),"
-				+ "foreign key (associe) references Associe(id));";
+		String ctX = "CREATE TABLE IF NOTE EXISTS X ("
+				+ ""
+				+ ");"; 
 		
 		try {
 			Statement s = connection.createStatement();
-			s.executeUpdate(ctLivre);
-			s.executeUpdate(ctExemplaire);
-			s.executeUpdate(ctAuteur);
-			s.executeUpdate(ctAssocie);
-			s.executeUpdate(ctPret);
-			
+			s.executeUpdate(ctX);			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -115,32 +75,12 @@ public class DBConnection {
 	 * @throws SQLException
 	 */
 	private void dbTuplesInit() {
-		String iLivre = "insert into Livre values"
-				+ "(1, 'Livre1', 1, " + Date.valueOf(LocalDate.now()) +"),"
-				+ "(2, 'Livre2', 1, " + Date.valueOf(LocalDate.of(2000, 11, 22)) +"),"
-				+ "(3, 'Le temps des tempêtes', 3, " + Date.valueOf(LocalDate.of(2021, 7, 16)) +");";
-			
-		String iExemplaire = "insert into Exemplaire values"
-				+ "(1, 2, 'blablabla Livre 2'),"
-				+ "(2, 2, 'blabla Livre 2'),"
-				+ "(3, 1, 'blablabla Livre 1');";
-		
-		String iAuteur = "insert into Auteur values"
-				+ "(1, 'Descartes', 'Rene'),"
-				+ "(2, 'Aristote', null),"
-				+ "(3, 'Mano', 'Aloe');";
-		
-		String iAssocie = "insert into Associe values"
-				+ "(1, 'Tokoyami', 'Towa'),"
-				+ "(2, 'Nakiri', 'Ayame'),"
-				+ "(3, 'Motoaki', 'Tanigo');";
-				
+		String iX = "INSERT INTO X VALUES"
+				+ "()";
+
 		try {
 			Statement s = connection.createStatement();
-			s.executeUpdate(iLivre);
-			s.executeUpdate(iExemplaire);
-			s.executeUpdate(iAuteur);
-			s.executeUpdate(iAssocie);
+			s.executeUpdate(iX);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();

161acc3012673768a4609571143c5139a95aab7f
Rezyia <fineladiesprotectress@gmail.com>
2021-12-14 17:40:23 +0100

    ajout insertion exemple

--- src/app/Application.java
+++ src/app/Application.java
@@ -6,6 +6,7 @@ public class Application {
 	public static void main(String [] args) {
 		System.out.println("Début du programme...");
 		DataBase db = new DataBase();
+		db.dbTuplesInit();
 		System.out.println("Fin du programme.");
 	}
 }
--- src/app/DataBase.java
+++ src/app/DataBase.java
@@ -16,8 +16,9 @@ public class DataBase {
 	public DataBase() {
 		connected = connect();
 		if (connected) {
+			dbDropTables();
 			dbTableInit();
-			//dbTuplesInit();	
+			//dbTuplesInit();
 		}
 	}
 	
@@ -50,9 +51,20 @@ public class DataBase {
 	}
 	
 
+	private void dbDropTables() {
+		String dropTables = "DROP TABLE IF EXISTS Plan, Candidature, Bourse, Enseignement, Etudiant, Enseignant CASCADE;";
+		
+		try {
+			Statement stmt = connection.createStatement();
+			stmt.execute(dropTables);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 	/**
-	 * DB tables initializer
+	 * Create tables in the DB
 	 * @throws SQLException
 	 */
 	private void dbTableInit() {		
@@ -60,8 +72,32 @@ public class DataBase {
 				+ "numero INT NOT NULL AUTO_INCREMENT,"
 				+ "nom VARCHAR(30) NOT NULL,"
 				+ "prenom VARCHAR(30) NOT NULL,"
-				+ "moyDernierSemestre DOUBLE(2,2),"
+				+ "moyDernierSemestre DECIMAL(4,2),"
 				+ "PRIMARY KEY (numero)"
+				+ ");";
+		
+		String ctEnseignant = "CREATE TABLE IF NOT EXISTS Enseignant ("
+				+ "id INT NOT NULL AUTO_INCREMENT,"
+				+ "nom VARCHAR(30) NOT NULL,"
+				+ "prenom VARCHAR(30) NOT NULL,"
+				+ "PRIMARY KEY (id)"
+				+ ");";
+		
+		String ctEnseignement = "CREATE TABLE IF NOT EXISTS Enseignement("
+				+ "id INT NOT NULL AUTO_INCREMENT,"
+				+ "intitule VARCHAR(50),"
+				+ "credits INT DEFAULT 0,"
+				+ "volumeHeures INT,"
+				+ "PRIMARY KEY (id)"
+				+ ");";
+		
+		String ctBourse = "CREATE TABLE IF NOT EXISTS Bourse("
+				+ "id INT NOT NULL AUTO_INCREMENT,"
+				+ "idResponsable INT NOT NULL,"
+				+ "destination VARCHAR(50),"
+				+ "nbPostes INT,"
+				+ "PRIMARY KEY (id),"
+				+ "FOREIGN KEY (idResponsable) REFERENCES Enseignant(id)"
 				+ ");";
 		
 		String ctCandidature = "CREATE TABLE IF NOT EXISTS Candidature ("
@@ -87,32 +123,6 @@ public class DataBase {
 				+ "FOREIGN KEY (idEnseignement) REFERENCES Enseignement(id)"
 				+ ");";
 		
-		String ctBourse = "CREATE TABLE IF NOT EXISTS Bourse("
-				+ "id INT NOT NULL AUTO_INCREMENT,"
-				+ "idResponsable INT NOT NULL,"
-				+ "destination VARCHAR(50),"
-				+ "nbPostes INT,"
-				+ "PRIMARY KEY (id),"
-				+ "FOREIGN KEY (idResponsable) REFERENCES Enseignant(id)"
-				+ ");";
-		
-		String ctEnseignant = "CREATE TABLE IF NOT EXISTS Enseignant ("
-				+ "id INT NOT NULL AUTO_INCREMENT,"
-				+ "nom VARCHAR(30) NOT NULL,"
-				+ "prenom VARCHAR(30) NOT NULL,"
-				+ "PRIMARY KEY (id)"
-				+ ");";
-		
-		
-		String ctEnseignement = "CREATE TABLE IF NOT EXISTS Enseignement("
-				+ "id INT NOT NULL AUTO_INCREMENT,"
-				+ "intitule VARCHAR(30),"
-				+ "credits INT DEFAULT 0,"
-				+ "volumeHeures INT,"
-				+ "PRIMARY KEY (id)"
-				+ ");";
-		
-		
 		try {
 			Statement s = connection.createStatement();
 			s.executeUpdate(ctEtudiant);
@@ -129,16 +139,45 @@ public class DataBase {
 	
 	
 	/**
-	 * DB tuples initializer
+	 * Insert tuples in the DB tables 
 	 * @throws SQLException
 	 */
-	private void dbTuplesInit() {
-		String iX = "INSERT INTO X VALUES"
-				+ "()";
+	public void dbTuplesInit() {
+		String iEtudiant = "INSERT INTO Etudiant(nom, prenom, moyDernierSemestre) VALUES"
+				+ "('Boulanger', 'Léo', 15.66),"	// id = 1
+				+ "('Boisseau', 'Théo', 20.00);";	// id = 2
+		
+		String iEnseignant = "INSERT INTO Enseignant(nom, prenom) VALUES"
+				+ "('Conte', 'Donatello'),"		// id=1
+				+ "('Ragot', 'Nicolas'),"		// id=2
+				+ "('Monmarché', 'Nicolas'),"	// id=3
+				+ "('Motoaki', 'Tanigo');";		// id=4
+		
+		String iEnseignement = "INSERT INTO Enseignement(intitule, credits, volumeHeures) VALUES"
+				+ "('Informatique', 4, 2000),"									// id = 1
+				+ "('Électronique et génie électrique', 2, 1900),"				// id = 2
+				+ "('Génie de l aménagement et de l environnement', 1, 1800),"	// id = 3
+				+ "('Mécanique et conception des systèmes', 3, 2000);";		// id = 4
+		
+		String iBourse = "INSERT INTO Bourse(idResponsable, destination, nbPostes) VALUES"
+				+ "(2, 'Montréal', 29),"	// id = 1
+				+ "(3, 'Akita', 42);";		// id = 2
+		
+		
+		String iCandidature = "INSERT INTO Candidature(etudiant, bourse, respLocal, respErasmus) VALUES" 
+				+ "();"; // Fait manuellement lors de l'exécution du programme 
+		
+		String iPlan = "INSERT INTO  VALUES"
+				+ "();"; // Fait manuellement lors de l'exécution du programme
+		
+
 
 		try {
 			Statement s = connection.createStatement();
-			s.executeUpdate(iX);
+			s.executeUpdate(iEtudiant);
+			s.executeUpdate(iEnseignant);
+			s.executeUpdate(iEnseignement);
+			s.executeUpdate(iBourse);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();

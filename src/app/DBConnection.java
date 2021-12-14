bb9b13ae5cef291e27c173956a16e4e2c5e131b5
Rezyia <fineladiesprotectress@gmail.com>
2021-12-14 12:57:28 +0100

    ajout init db

--- src/app/DBConnection.java
+++ src/app/DBConnection.java
@@ -56,13 +56,71 @@ public class DBConnection {
 	 * @throws SQLException
 	 */
 	private void dbTableInit() {		
-		String ctX = "CREATE TABLE IF NOTE EXISTS X ("
-				+ ""
-				+ ");"; 
+		String ctEtudiant = "CREATE TABLE IF NOT EXISTS Etudiant ("
+				+ "numero INT NOT NULL AUTO_INCREMENT,"
+				+ "nom VARCHAR(30) NOT NULL,"
+				+ "prenom VARCHAR(30) NOT NULL,"
+				+ "moyDernierSemestre DOUBLE(2,2),"
+				+ "PRIMARY KEY (numero)"
+				+ ");";
+		
+		String ctCandidature = "CREATE TABLE IF NOT EXISTS Candidature ("
+				+ "id INT NOT NULL AUTO_INCREMENT,"
+				+ "etudiant INT NOT NULL,"
+				+ "bourse INT NOT NULL,"
+				+ "respLocal int NOT NULL,"
+				+ "respErasmus int NOT NULL,"
+				+ "noteLocal DOUBLE(2,2),"
+				+ "noteErasmus DOUBLE(2,2),"
+				+ "PRIMARY KEY (id),"
+				+ "FOREIGN KEY (etudiant) references Etudiant(numero),"
+				+ "FOREIGN KEY (bourse) references Bourse(id),"
+				+ "FOREIGN KEY (respLocal) references Enseignant(id),"
+				+ "FOREIGN KEY (respErasmus) references Enseignant(id)"
+				+ ");";
+		
+		String ctPlan = "CREATE TABLE IF NOT EXISTS Plan("
+				+ "idCandidature INT NOT NULL,"
+				+ "idEnseignement INT NOT NULL,"
+				+ "PRIMARY KEY (idCandidature, idEnseignement),"
+				+ "FOREIGN KEY (idCandidature) REFERENCES Candidature(id),"
+				+ "FOREIGN KEY (idEnseignement) REFERENCES Enseignement(id)"
+				+ ");";
+		
+		String ctBourse = "CREATE TABLE IF NOT EXISTS Bourse("
+				+ "id INT NOT NULL AUTO_INCREMENT,"
+				+ "idResponsable INT NOT NULL,"
+				+ "destination VARCHAR(50),"
+				+ "nbPostes INT,"
+				+ "PRIMARY KEY (id),"
+				+ "FOREIGN KEY (idResponsable) REFERENCES Enseignant(id)"
+				+ ");";
+		
+		String ctEnseignant = "CREATE TABLE IF NOT EXISTS Enseignant ("
+				+ "id INT NOT NULL AUTO_INCREMENT,"
+				+ "nom VARCHAR(30) NOT NULL,"
+				+ "prenom VARCHAR(30) NOT NULL,"
+				+ "PRIMARY KEY (id)"
+				+ ");";
+		
+		
+		String ctEnseignement = "CREATE TABLE IF NOT EXISTS Enseignement("
+				+ "id INT NOT NULL AUTO_INCREMENT,"
+				+ "intitule VARCHAR(30),"
+				+ "credits INT DEFAULT 0,"
+				+ "volumeHeures INT,"
+				+ "PRIMARY KEY (id)"
+				+ ");";
+		
 		
 		try {
 			Statement s = connection.createStatement();
-			s.executeUpdate(ctX);			
+			s.executeUpdate(ctEtudiant);			
+			s.executeUpdate(ctCandidature);			
+			s.executeUpdate(ctEnseignant);			
+			s.executeUpdate(ctEnseignement);			
+			s.executeUpdate(ctBourse);			
+			s.executeUpdate(ctPlan);			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
